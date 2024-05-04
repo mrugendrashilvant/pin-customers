5fe6a87d4168b0bc23fba2db49d4f9229ea382dd
Mrugendra Shilvant <mrugendra.shilvant@digio.in>
2024-05-04 08:39:55 +0530

    added enums interfaces and basic table

---
+++ src/app/pin/create-pin/create-pin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-pin',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-pin.component.html',
+  styleUrl: './create-pin.component.scss'
+})
+export class CreatePinComponent {
+
+}
---
+++ src/app/pin/pin-list/pin-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pin-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './pin-list.component.html',
+  styleUrl: './pin-list.component.scss'
+})
+export class PinListComponent {
+
+}
